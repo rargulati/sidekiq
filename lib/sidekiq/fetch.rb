1d72c66e9ad6b1a95baddd1ac2034b72442d6998
Adam Blaschke <penguinxr2@gmail.com>
2012-10-25 11:57:52 -0400

    don't auto-delete queues, removed dubug messages

--- lib/sidekiq/fetch.rb
+++ lib/sidekiq/fetch.rb
@@ -15,9 +15,8 @@ module Sidekiq
     def initialize(mgr, queues, strict, ignored_queues=[])
       @mgr = mgr
       @strictly_ordered_queues = strict
-      @queues = queues.map { |q| "queue:#{q}" }
+      @queues = (queues-ignored_queues).map { |q| "queue:#{q}" }
       @unique_queues = @queues.uniq
-      @ignored_queues = ignored_queues
     end
 
     # Fetching is straightforward: the Manager makes a fetch
@@ -83,19 +82,6 @@ module Sidekiq
         queue=@queues.pop
         @queues.insert(0,queue)
         queues=@queues.dup
-        puts "Popping #{queue}, order: #{queues.join(",")}"
-        #clean out empty queues
-        
-          Sidekiq.redis do |conn|
-                queues.each { |q|
-                  if conn.llen(q) == 0
-                    Sidekiq.redis do |conn|
-                      conn.del(q)
-                      conn.srem("queues",q.gsub("queue:",""))
-                    end
-                  end
-                }
-          end
         else
           queues=["nil"]
         end
